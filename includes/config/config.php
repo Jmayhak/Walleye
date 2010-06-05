a07e70e322c02cd59cbb38bc40e47f8f49bae8e3
Jonathan Mayhak <jmayhak@gmail.com>
2010-06-05 13:54:33 -0400

    models/controllers/views/static content are no longer defined in the appOptions array. This is now either directly required in Walleye (models/ controlers) or set as a constant in Walleye (static/views)

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,33 +1,9 @@
 <?php
 
-$wConfigOptions = array(
-    'BASE' => '',
+$wAppOptions = array(
+    'BASE' => '/var/www/vhosts/veschile.com/subdomains/efnep/',
     'PRODUCTION' => true,
-    'LOCAL' => false,
-    //Define the models found in includes/app/models/
-    'MODELS' => array(
-        'USER' => 'user.php'
-    ),
-    //Define the controllers found in includes/app/controllers/
-    'CONTROLLERS' => array(
-        'USER' => 'user.php',
-        'API' => 'api.php'
-    ),
-    //Define the views found in includes/app/views/
-    'VIEWS' => array(
-        'BASE_HEADER_VIEW' => '_header.php',
-        'BASE_FOOTER_VIEW' => '_footer.php',
-        'BASE_INDEX_VIEW' => 'index.php',
-        'LOGIN_VIEW' => 'user/login.php',
-        'MOBILE_VIEW' => 'mobile/mobile.php'
-    ),
-    //Define the static files found in httpdocs/
-    'STATIC' => array(
-        'DEFAULT_STYLESHEET' => '/css/default.css',
-        'PQP_OVERLAY' => '/plugins/pqp/overlay.gif',
-        'PQP_CSS' => '/plugins/pqp/pqp.css',
-        'PQP_SIDE' => '/plugins/pqp/side.png'
-    )
+    'LOCAL' => false
 );
 
 ?>
