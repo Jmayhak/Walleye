7975c4160b42e47d61b2e34a6b11e4d911864bf8
Jonathan Mayhak <jmayhak@gmail.com>
2010-06-05 13:49:15 -0400

    used the correct functions to set the application config options

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -6,9 +6,9 @@ require('../includes/config/db.php');
 require('../includes/core/walleye.php');
 
 $app = Walleye::getInstance();
-$app->setOptions = $wConfigOptions;
-$app->setRoutes = $wRoutes;
-$app->dbOptions = $wDbConfig;
+$app->setAppOptions($wAppOptions);
+$app->setRoutes($wRoutes);
+$app->setDbOptions($wDbConfig);
 $app->run();
 
 ?>
