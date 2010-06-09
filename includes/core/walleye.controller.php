85a5c1177e37d739ac4f02178f1031dfdc0f750a
Jonathan Mayhak <jmayhak@gmail.com>
2010-06-09 19:50:59 -0400

    added getHandler() function to perform a regexp match on the url to decide which handler in the controller should be called

--- includes/core/walleye.controller.php
+++ includes/core/walleye.controller.php
@@ -5,7 +5,7 @@
  *
  * Every controller in your application should extend this class. This is because the routes() function
  * in Walleye tries to call the doAction() function dynamically (it doesn't know what class it's calling
- * that function for). 
+ * that function for).
  *
  * @author Jonathan Mayhak <Jmayhak@gmail.com>
  * @version 0.5
@@ -16,54 +16,110 @@ abstract class Walleye_controller {
     /**
      * Either the GET or POST array
      * @var array
+     * @access protected
      */
-    protected $data;
+    protected $data = array();
 
     /**
-     * The action given in the URL
+     * the URL
+     * @var string
+     * @access protected
+     */
+    protected $url;
+
+    /**
+     * The routes the controller that extends this abstract class should follow
      * @var array
+     * @access protected
      */
-    protected $action;
+    protected $handlers = array();
 
     /**
+     * Note: when handlers array is created do not forget to define a default handler called 'default'
+     *
      * @abstract
+     * @access protected
      * @param array $data
-     * @param array $action
-     * @return self
+     * @param string $url
      */
-    abstract public function __construct($data, $action);
+    abstract protected function __construct($url, $data);
 
     /**
+     * Since Walleye is based on PHP 5.1.6 we cannot perform late static binding and stick doAction and
+     * getHandler together. Look for an update in future versions of Walleye.
+     *
      * @abstract
+     * @access protected
      * @return void
      */
-    abstract public function doAction();
+    abstract protected function doAction();
+
+    /**
+     * Call this function first in the doAction() function to figure out which handler (function in the controller)
+     * should be called.
+     *
+     * @final
+     * @see Walleye_controller::$url
+     * @see Walleye_controller::$handlers
+     * @return string|null
+     */
+    final protected function getHandler() {
+        foreach ($this->handlers as $route => $handler) {
+            if ($route == 'default') {
+                return $handler;
+            }
+            else {
+                if (preg_match($route, $this->url)) {
+                    return $handler;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This function will tell the view() function to display the 404 page which defaults to 404.php
+     *
+     * @see Walleye_controller::view()
+     * @access protected
+     * @return void
+     */
+    protected function error_404($view = '404.php') {
+        $this->view($view, array());
+    }
 
     /**
      * Redirects the browser to a specific URL. MUST be called before the <html>
      * is sent to the browser (before view())
      *
-     * @param string $URL the URL the browser should be redirected to. Send the full URL
+     * @final
+     * @access protected
      * @see Walleye_controller::view()
+     * @param string $URL the URL the browser should be redirected to. Send the full URL
      * @return void
      */
-    final public function redirect($URL) {
+    final protected function redirect($URL) {
         header("Location: $URL");
         exit();
     }
 
     /**
-     *  Render a template
+     *  Render a template. Be sure to include the path from /includes/app/views/
      *
+     * ex. view('home/index.php', array('title'=>'Title'));
+     *
+     * @final
+     * @access protected
      * @param string $view the view to be rendered.
      * @param array $values the values to be shown on the view
+     * @param string $dir the location from includes/app/views/
      * @return void
      */
-    final public function view($view = 'index.php', $values = array()) {
+    final protected function view($view, $values = array()) {
         // TODO either track views in db here or as a trigger
-        // TODO make the default view shown a 404 page
-        include(Walleye::getServerBaseDir() . "includes/app/views/$view");
-        exit();
+        Console::log('view: ' . $view);
+        Console::log('values: ' . $values);
+        include(Walleye::getServerBaseDir() . 'includes/app/views/' . $view);
     }
 }
 
