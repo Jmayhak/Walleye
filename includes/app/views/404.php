7bbb61436d0833fbc2e6e449804a99d034a1f7b3
Jonathan Mayhak <jmayhak@gmail.com>
2010-06-16 11:22:28 -0400

    added a 404 page

--- includes/app/views/404.php
+++ includes/app/views/404.php
@@ -1,6 +1,6 @@
-<?php require('_header.php') ?>
+<?php include('_header.php') ?>
 
 <h2>404 Error</h2>
 <p>Maybe you just wanted to go to the <a href='/' target='self'>home page</a>?</p>
 
-<?php require('_footer.php') ?>
+<?php include('_footer.php') ?>
