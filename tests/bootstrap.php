c109d9ff416cf4a04e11ed3ada238a7472e4f78b
Jonathan Mayhak <jmayhak@gmail.com>
2010-07-08 10:51:28 -0400

    mulligan

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,3 @@
 <?php
 
-$wAppOptions = array(
-    'BASE' => '',
-    'PRODUCTION' => true,
-    'LOCAL' => false,
-);
-
-$wRoutes = array(
-    '//login/' => 'cUser',
-    '//logout/' => 'cUser',
-    '//api/' => 'cApi',
-    'default' => 'cSite'
-);
-
-require('../includes/core/walleye.php');
-
-$app = Walleye::getInstance();
-$app->setAppOptions($wAppOptions);
-$app->setRoutes($wRoutes);
-$app->setDbOptions($wDbConfig);
-$app->run();
-
 ?>
