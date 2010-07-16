501e299fedb63754188156017195eada1b74f55f
Jonathan Mayhak <jmayhak@gmail.com>
2010-07-16 11:56:27 -0400

    added gritter alert logging

--- includes/app/views/_header.php
+++ includes/app/views/_header.php
@@ -4,6 +4,9 @@
     <title></title>
     <meta name="keywords" content=""/>
     <meta name="description" content=""/>
+    <link href="/css/jquery.gritter.css" rel="stylesheet" media="all"/>
+    <script type="text/javascript" src="/js/jquery-1.3.2.js"></script>
+    <script type="text/javascript" src="/js/jquery.gritter.min.js"></script>
     <?php if (isset($values['css'])) : foreach ($values['css'] as $src) : ?>
             <script type="text/javascript" src="<?php echo $src; ?>"></script>
     <?php endforeach; endif; ?>
@@ -12,14 +15,27 @@
     <?php endforeach; endif; ?>
     <script type="text/javascript">
         <?php if (isset($values['logs'])) : foreach ($values['logs'] as $log) : ?>
-            <?php if ($log['type'] == 'error') : ?>
+                $(document).ready(function() {
+                <?php if ($log['type'] == 'error') : ?>
                 console.error('<?php echo $log['message'] . ' ' . $log['file'] . ' ' . $log['line']; ?>');
-            <?php else : ?>
+            <?php elseif ($log['type'] == 'alert') : ?>
+                        $.gritter.add({
+                            title: 'Alert',
+                            text: '<?php echo $log['message'] ?>',
+                            image: '',
+                            sticky: false,
+                            time: ''
+                        });
+
+                    return false;
+                <?php  else : ?>
                 console.log('<?php echo $log['message'] . ' ' . $log['file'] . ' ' . $log['line']; ?>');
             <?php endif; ?>
+
+                });
+
         <?php endforeach; endif; ?>
     </script>
 </head>
 <body>
-<div id="wrapper">
     
