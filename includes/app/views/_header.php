010e341c5c28d686c4a20860acbff372822c222b
Jonathan Mayhak <jmayhak@gmail.com>
2010-07-16 12:07:33 -0400

    each log messages does not create a new $(document).ready() anymore

--- includes/app/views/_header.php
+++ includes/app/views/_header.php
@@ -7,16 +7,17 @@
     <link href="/css/jquery.gritter.css" rel="stylesheet" media="all"/>
     <script type="text/javascript" src="/js/jquery-1.3.2.js"></script>
     <script type="text/javascript" src="/js/jquery.gritter.min.js"></script>
-    <?php if (isset($values['css'])) : foreach ($values['css'] as $src) : ?>
-            <script type="text/javascript" src="<?php echo $src; ?>"></script>
-    <?php endforeach; endif; ?>
-    <?php if (isset($values['js'])) : foreach ($values['js'] as $js) : ?>
-            <script rel="stylesheet" href="<?php echo $js; ?>"></script>
-    <?php endforeach; endif; ?>
+<?php if (isset($values['css'])) : foreach ($values['css'] as $src) : ?>
+    <script type="text/javascript" src="<?php echo $src; ?>"></script>
+<?php endforeach; endif; ?>
+<?php if (isset($values['js'])) : foreach ($values['js'] as $js) : ?>
+    <script rel="stylesheet" href="<?php echo $js; ?>"></script>
+<?php endforeach; endif; ?>
     <script type="text/javascript">
+        $(document).ready(function() {
         <?php if (isset($values['logs'])) : foreach ($values['logs'] as $log) : ?>
-                $(document).ready(function() {
-                <?php if ($log['type'] == 'error') : ?>
+
+        <?php if ($log['type'] == 'error') : ?>
                 console.error('<?php echo $log['message'] . ' ' . $log['file'] . ' ' . $log['line']; ?>');
             <?php elseif ($log['type'] == 'alert') : ?>
                         $.gritter.add({
@@ -26,15 +27,14 @@
                             sticky: false,
                             time: ''
                         });
-
-                    return false;
                 <?php  else : ?>
                 console.log('<?php echo $log['message'] . ' ' . $log['file'] . ' ' . $log['line']; ?>');
             <?php endif; ?>
 
-                });
+
 
         <?php endforeach; endif; ?>
+        });
     </script>
 </head>
 <body>
