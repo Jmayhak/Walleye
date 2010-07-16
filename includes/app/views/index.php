400c08567c36148d7cbcb854ea2295281a49eb83
Jonathan Mayhak <jmayhak@gmail.com>
2010-07-16 12:25:34 -0400

    added a little information about walleye to the example page

--- includes/app/views/index.php
+++ includes/app/views/index.php
@@ -1,13 +1,11 @@
 <?php $this->view('_header.php'); ?>
-<div id="wrapper">
-<div id="container">
-    <?php if (Walleye_user::getLoggedUser()) : ?>
-        <p>Looks like everything is working</p>
-        <a href="/user/logout">Logout</a>
-    <?php else : ?>
-        <p>Try logging in</p>
-        <a href="/user/login">Login</a>
-    <?php endif; ?>
-</div>
-</div>
+<h2>Walleye - An MVC style framework for PHP 5.1.6</h2>
+<p>Fork it <a href="http://github.com/Jmayhak/Walleye">here</a></p>
+<?php if (Walleye_user::getLoggedUser()) : ?>
+<p>Looks like everything is working</p>
+<a href="/user/logout">Logout</a>
+<?php else : ?>
+<p>Try logging in</p>
+<a href="/user/login">Login</a>
+<?php endif; ?>
 <?php $this->view('_footer.php'); ?>
