18bd0ffb0dd18073051491cfe8da583e2144d542
Jonathan Mayhak <jmayhak@gmail.com>
2010-07-16 11:58:28 -0400

    added phpdoc info for two more params in the log function

--- includes/core/walleye.console.php
+++ includes/core/walleye.console.php
@@ -6,27 +6,22 @@
  * for storage will be saved to /logs/access.log
  *
  * @author Jonathan Mayhak <Jmayhak@gmail.com>
- * @version 0.8
+ * @version 0.5
  * @package Walleye
  */
 class Console {
-        
+
     /**
      * Holds all logs that are going to be sent to the browser
      * @var array
      * @access protected
      */
     protected static $logs = array();
-        
+
     /**
      * Use this function to add a general log
-     * @static
-     * @param string $message the message to be logged
-     * @param string $file the file this log occurred in
-     * @param string $line the line this log occurred on
-     * @param boolean $store whether or not to store this log in /logs/app.log
-     * @param string $type the type of log
-     * @return void
+     * @param string $message
+     * @param Exception $ex
      */
     public static function log($message, $file = 'unknown file', $line = 'unknown line', $store = true, $type = 'log') {
         $logItem = array(
