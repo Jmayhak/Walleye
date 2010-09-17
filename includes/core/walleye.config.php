9342f51940e56651254106e72c975b97f3a4fdb5
Jonathan Mayhak <jmayhak@gmail.com>
2010-09-17 17:21:29 -0500

    added redirect if mobile option. only checks for iphone

--- includes/core/walleye.config.php
+++ includes/core/walleye.config.php
@@ -19,13 +19,13 @@ class Walleye_config {
         return array(
                    // the location of this application from the server root. end with a '/'
                    'BASE' => '',
+                   // leave blank to not forward anywhere. only checks for iphone
+				   'IF_MOBILE_REDIRECT' => ''
                    // end the domain with a '/'
                    'DOMAIN' => '',
                    // if in production mode, no php warning/errors will be shown
                    'PRODUCTION' => false,
                    'LOG_ERRORS' => true,
-                   // the location of the log file in relation to BASE
-                   'LOG_FILE' => 'logs/app.log',
                    // Enter the expiration time in days
                    'REG_KEY_EXPIRE_TIME' => '7', // not implemented
                    // The length a session lasts in code in days (php.ini controls the session variable)
@@ -53,6 +53,7 @@ class Walleye_config {
      * @return array
      */
     public static function getRoutes() {
+	    // the key is a regexp and the value is the name of the controller class
         return array(
                    '/^(\/user)/' => 'User',
                    '/^(\/api)/' => 'Api',
@@ -66,11 +67,9 @@ $appOptions = Walleye_config::getAppOptions();
 // PRODUCTION
 if ($appOptions['PRODUCTION']) {
     // Turn off all error reporting
-    ini_set("display_errors", 0);
+    ini_set('display_errors', 0);
 }
 // DEVELOPMENT
 else {
-
+    ini_set('display_errors', 1)
 }
-
-?>
