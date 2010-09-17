742dbc503841f4ff3157499d18cda5457abc65f6
Jonathan Mayhak <jmayhak@gmail.com>
2010-09-17 17:15:45 -0500

    logs are no longer added to the /logs/app.log file. they are now all stored in the database in a Logs table

--- includes/core/walleye.console.php
+++ includes/core/walleye.console.php
@@ -1,12 +1,12 @@
 <?php
+
 /*
  * walleye.console.php
  *
  * Used for general logging and logging errors. All errors that are marked
- * for storage will be saved to /logs/access.log
+ * for storage will be saved to the Logs table in the db
  *
  * @author Jonathan Mayhak <Jmayhak@gmail.com>
- * @version 0.5
  * @package Walleye
  */
 class Console {
@@ -21,15 +21,16 @@ class Console {
     /**
      * Use this function to add a general log
      * @param string $message
-     * @param Exception $ex
+     * @param string $file
+     * @param string $line
+     * @param boolean $store
      */
     public static function log($message, $file = 'unknown file', $line = 'unknown line', $store = true, $type = 'log') {
         $logItem = array(
             'type' => $type,
             'message' => $message,
             'file' => $file,
-            'line' => $line,
-            'date' => date('F j, Y, g:i a')
+            'line' => $line
         );
         $options = Walleye_config::getAppOptions();
         if ($store && $options['LOG_ERRORS']) {
@@ -41,25 +42,16 @@ class Console {
     /**
      * Use this function to add an error log to the app.log file
      * @param string $message
-     * @param exception $ex
+     * @param string $file
+     * @param string $line
      * @param boolean $store
      */
-    public static function logError($message, $ex = null, $store = true) {
-        if (is_null($ex)) {
-            Console::log($message, 'unknown file', 'unknown line', $store, 'error');
-        }
-        else if (class_exists($ex)) {
-            if (in_array('getFile', get_class_methods($ex)) && in_array('getLine', get_class_methods($ex))) {
-                Console::log($message, $ex->getFile(), $ex->getLine(), $store, 'error');
-            }
-        }
-        else {
-            Console::log($message, 'unknown file', 'unknown line', $store, 'error');
-        }
+    public static function logError($message, $file = 'unknown file', $line = 'unknown line', $store = true) {
+        Console::log($message, $file, $line, $store, 'error');
     }
 
     /**
-     * Use this function to alert the currently logged in user of something
+     * Use this function to alert the currently logged in user of something via gritter
      * @static
      * @param string $message
      * @param string $file
@@ -80,20 +72,15 @@ class Console {
     }
 
     /**
-     * Takes an array and adds it to the logs/app.log file
-     * @param array $log
-     * @param string $location
-     * @return void
+     * Takes an array representing a log and inserts it into the Logs table in the db
+     * @param array $logItem
+     * @return boolean
      */
-    private static function storeLog($logItem, $location = null) {
-        if (is_null($location)) {
-            $options = Walleye_config::getAppOptions();
-            $location = $options['LOG_FILE'];
-        }
-        $stream = fopen(Walleye::getServerBaseDir() . $location, 'a');
-        fwrite($stream, print_array($logItem) . "\n");
-        fclose($stream);
+    private static function storeLog($logItem) {
+        $db = new Walleye_database();
+        $insert_log_stmt = $db->prepare('INSERT INTO Logs (user_id, type, line, file, message) VALUES (?, ?, ?, ?, ?)');
+        $user_id = (is_null(Walleye_user::getLoggedUser())) ? 0 : Walleye_user::getLoggedUser()->getId();
+        $insert_log_stmt->bind_param('issss', $user_id, $logItem['type'], $logItem['line'], $logItem['file'], $logItem['message']);
+        return $insert_log_stmt->execute();
     }
 }
-
-?>
--- walleye.sql
+++ walleye.sql
@@ -1,13 +1,36 @@
-# Dump of table Roles
+# Sequel Pro dump
+# Version 2492
+# http://code.google.com/p/sequel-pro
+#
+# Host: 127.0.0.1 (MySQL 5.0.22)
+# Database: walleye
+# Generation Time: 2010-09-17 17:13:14 -0500
+# ************************************************************
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table Logs
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Roles`;
+DROP TABLE IF EXISTS `Logs`;
 
-CREATE TABLE `Roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Logs` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `type` varchar(5) NOT NULL default 'LOG',
+  `line` varchar(255) default NULL,
+  `file` text,
+  `date_created` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `message` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
@@ -18,24 +41,10 @@ CREATE TABLE `Roles` (
 DROP TABLE IF EXISTS `Sessions`;
 
 CREATE TABLE `Sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `session_key` varchar(255) NOT NULL,
-  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
-
-
-
-# Dump of table UserRoles
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `UserRoles`;
-
-CREATE TABLE `UserRoles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
@@ -46,14 +55,14 @@ CREATE TABLE `UserRoles` (
 DROP TABLE IF EXISTS `Users`;
 
 CREATE TABLE `Users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL auto_increment,
+  `first_name` varchar(255) default NULL,
+  `last_name` varchar(255) default NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
 
@@ -63,9 +72,22 @@ CREATE TABLE `Users` (
 DROP TABLE IF EXISTS `UserSessions`;
 
 CREATE TABLE `UserSessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `session_id` int(11) NOT NULL,
-  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
+  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
