f4930d3db43197cd250a0236f5e455bfaea00d24
Jonathan Mayhak <jmayhak@gmail.com>
2010-10-05 14:01:49 -0500

    renamed the original encode function to hash_data and updated its phpdoc.
    added an encode and decode function

--- includes/core/walleye.functions.php
+++ includes/core/walleye.functions.php
@@ -17,18 +17,58 @@ function __autoload($class_name) {
 }
 
 /**
- * Takes a 1 dimensional array of data and returns a 256bit string hash whirlpool style
+ * Takes a string and returns a 256bit string hash whirlpool style
  *
  * @final
  * @access protected
  * @param array $data
  * @return string|null
  */
-function encode($data) {
+function hash_data($data) {
     if (is_array($data)) {
         return null;
     }
     return hash('whirlpool', $data);
+}
+
+/**
+ * Pass the string to encode and the secret key as a salt
+ * @see decrypt
+ * @param string $string
+ * @param string $key
+ * @return string
+ */
+function encrypt($string, $key) {
+    $result = '';
+    for($i=0; $i<strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key))-1, 1);
+      $char = chr(ord($char)+ord($keychar));
+      $result.=$char;
+    }
+
+    return base64_encode($result);
+  }
+
+/**
+ * Pass the encrypted string and the secret key to decrypt
+ * @see encrypt
+ * @param string $string
+ * @param string $key
+ * @return string
+ */
+function decrypt($string, $key) {
+    $result = '';
+    $string = base64_decode($string);
+  
+    for($i=0; $i<strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key))-1, 1);
+      $char = chr(ord($char)-ord($keychar));
+      $result.=$char;
+    }
+  
+    return $result;
 }
 
 /**
