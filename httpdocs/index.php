7e2b5cc854bba59ef461486dcd59178c652d3344
Jonathan Mayhak <jmayhak@gmail.com>
2010-10-05 13:59:02 -0500

    included fpdf and the rest api helper class to the example app

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -2,6 +2,12 @@
 
 require('../includes/core/walleye.php');
 
+// include libraries
+require('../includes/core/libraries/RestRequest.inc.php');
+$appOptions = Walleye_config::getAppOptions();
+define('FPDF_FONTPATH', $appOptions['BASE'] . 'includes/core/libraries/font/');
+require('../includes/core/libraries/fpdf.php');
+
 $app = Walleye::getInstance();
 $app->run();
 
