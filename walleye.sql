315c30b94e41f9d5f51df5a089a4ce104d3e1529
Jonathan Mayhak <jmayhak@gmail.com>
2010-10-05 14:03:23 -0500

    updated the sql to include a keys table which has fields for api key and seret key. also added a table called userkeys to map a user to a key pair.
    this table should be used with a public api

--- walleye.sql
+++ walleye.sql
@@ -2,9 +2,9 @@
 # Version 2492
 # http://code.google.com/p/sequel-pro
 #
-# Host: 127.0.0.1 (MySQL 5.0.22)
+# Host: 127.0.0.1 (MySQL 5.1.41-3ubuntu12.6)
 # Database: walleye
-# Generation Time: 2010-09-17 17:13:14 -0500
+# Generation Time: 2010-10-05 13:31:25 -0500
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,20 +17,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+# Dump of table Keys
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Keys`;
+
+CREATE TABLE `Keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `secret_key` varchar(255) DEFAULT NULL,
+  `api_key` varchar(255) DEFAULT NULL,
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
 # Dump of table Logs
 # ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `Logs`;
 
 CREATE TABLE `Logs` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `type` varchar(5) NOT NULL default 'LOG',
-  `line` varchar(255) default NULL,
+  `type` varchar(5) NOT NULL DEFAULT 'LOG',
+  `line` varchar(255) DEFAULT NULL,
   `file` text,
-  `date_created` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `message` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
@@ -41,10 +56,25 @@ CREATE TABLE `Logs` (
 DROP TABLE IF EXISTS `Sessions`;
 
 CREATE TABLE `Sessions` (
-  `id` int(11) NOT NULL auto_increment,
-  `session_key` varchar(255) NOT NULL,
-  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_key` varchar(255) NOT NULL DEFAULT '',
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table UserKeys
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `UserKeys`;
+
+CREATE TABLE `UserKeys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `key_id` int(11) DEFAULT NULL,
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
@@ -55,13 +85,13 @@ CREATE TABLE `Sessions` (
 DROP TABLE IF EXISTS `Users`;
 
 CREATE TABLE `Users` (
-  `id` int(11) NOT NULL auto_increment,
-  `first_name` varchar(255) default NULL,
-  `last_name` varchar(255) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
@@ -72,11 +102,11 @@ CREATE TABLE `Users` (
 DROP TABLE IF EXISTS `UserSessions`;
 
 CREATE TABLE `UserSessions` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `session_id` int(11) NOT NULL,
-  `date_created` timestamp NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
