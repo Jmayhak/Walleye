a6028b7174e7e6ea748252ecfe297909afcd72db
Jason Rikard <jsnrkd@gmail.com>
2011-06-15 17:41:55 -0400

    Made the abstract model class an interface to comply with PHP. Changed the appropriate keywords from extends to implements in the classes that were looking to extend the old class. Finished implementing the overridden error_404 method in the example site.php controller to follow the expected abstracted impelementation.

--- includes/core/walleye.model.php
+++ includes/core/walleye.model.php
@@ -10,7 +10,7 @@ namespace Walleye;
  * @author	Jonathan Mayhak <Jmayhak@gmail.com>
  * @package	Walleye
  */
-abstract class Model {
+interface Model {
 
     /**
      * Every model in your application should have a constructor that takes an id to initialize. All
@@ -21,7 +21,7 @@ abstract class Model {
      * @param int|string $id
      * @return self
      */
-    abstract public static function withId($id);
+    public static function withId($id);
 
     /**
      * After you are done setting properties in your model call this function. Implement this function
@@ -30,7 +30,7 @@ abstract class Model {
      * @abstract
      * @return void
      */
-    abstract public function commit();
+    public function commit();
 
     /**
      * Use this function to create a new row in the database for this instance of this model.
@@ -38,7 +38,7 @@ abstract class Model {
      * @abstract
      * @return self
      */
-    abstract public static function create();
+    public static function create();
 
     /**
      * Every model in your application should have a toString function. 5.1.6 does not support __toString
@@ -46,6 +46,6 @@ abstract class Model {
      * @abstract
      * @return string
      */
-    abstract public function __toString();
+    public function __toString();
 
 }
